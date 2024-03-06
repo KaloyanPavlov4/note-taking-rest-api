9799dd1aac84e668edde6d90ce9b9855175fb950
Kaloyan Pavlov <kaloyanpavlov4@gmail.com>
2024-03-06 10:24:34 +0200

    Replace authentication with authenticated user's username

--- src/main/java/com/kaloyan/notetakingapp/service/NoteService.java
+++ src/main/java/com/kaloyan/notetakingapp/service/NoteService.java
@@ -13,9 +13,9 @@ public interface NoteService {
 
     Flux<NoteDTO> findAll(Pageable pageable);
 
-    Mono<NoteDTO> save(NoteDTO noteDTO, Authentication authentication);
+    Mono<NoteDTO> save(NoteDTO noteDTO, Mono<String> authenticatedUsername);
 
-    Mono<NoteDTO> edit(UUID uuid, NoteDTO noteDTO, Authentication authentication);
+    Mono<NoteDTO> edit(UUID uuid, NoteDTO noteDTO, Mono<String> authenticatedUsername);
 
-    Mono<Void> deleteById(UUID uuid, Authentication authentication);
+    Mono<Void> deleteById(UUID uuid, Mono<String> authenticatedUsername);
 }
