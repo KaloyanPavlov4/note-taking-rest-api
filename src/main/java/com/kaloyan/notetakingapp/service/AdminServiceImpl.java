3989f250458c2753ed56a98884e1762e36104756
Kaloyan Pavlov <kaloyanpavlov4@gmail.com>
2024-03-06 10:34:38 +0200

    Add Service implementations

---
+++ src/main/java/com/kaloyan/notetakingapp/service/AdminServiceImpl.java
@@ -0,0 +1,46 @@
+package com.kaloyan.notetakingapp.service;
+
+import com.kaloyan.notetakingapp.dto.UserDTO;
+import com.kaloyan.notetakingapp.model.Role;
+import com.kaloyan.notetakingapp.repository.NoteRepository;
+import com.kaloyan.notetakingapp.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.util.UUID;
+
+@Service
+public class AdminServiceImpl implements AdminService{
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    NoteRepository noteRepository;
+
+    @Override
+    public Flux<Void> deleteUser(UUID userId) {
+        return userRepository.findById(userId).flatMapMany(user -> {
+            if(user.getRole().equals(Role.ROLE_ADMIN)){
+                throw new AccessDeniedException("Admins cannot delete other admins!");
+            }
+            return Flux.merge(noteRepository.deleteAllNotesByUser(userId), userRepository.deleteById(userId));
+        });
+    }
+
+    @Override
+    public Mono<Void> deleteNote(UUID noteId) {
+        return noteRepository.deleteById(noteId);
+    }
+
+    @Override
+    public Mono<UserDTO> makeUserAdmin(UUID userId) {
+        return userRepository.findById(userId).flatMap(user -> {
+            user.setRole(Role.ROLE_ADMIN);
+            return userRepository.save(user).map(UserDTO::new);
+        });
+    }
+}
---
+++ src/main/java/com/kaloyan/notetakingapp/service/UserServiceImpl.java
@@ -0,0 +1,81 @@
+package com.kaloyan.notetakingapp.service;
+
+import com.kaloyan.notetakingapp.dto.UserDTO;
+import com.kaloyan.notetakingapp.exception.DifferentUserException;
+import com.kaloyan.notetakingapp.model.Role;
+import com.kaloyan.notetakingapp.model.User;
+import com.kaloyan.notetakingapp.repository.NoteRepository;
+import com.kaloyan.notetakingapp.repository.UserRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.util.UUID;
+
+@AllArgsConstructor
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    NoteRepository noteRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    private Mono<UserDTO> userWithNotes(User user) {
+        return noteRepository.findAllByUser(user.getId()).collectList().flatMap(notes -> {
+            user.setNotes(notes);
+            return Mono.just(new UserDTO(user));
+        });
+    }
+
+    @Override
+    public Mono<UserDTO> findById(UUID uuid) {
+        return userRepository.findById(uuid).flatMap(this::userWithNotes);
+    }
+
+    @Override
+    public Flux<UserDTO> findAll(Pageable pageable) {
+        return userRepository.findAll().flatMap(this::userWithNotes)
+                .skip(pageable.getPageNumber() * pageable.getPageSize()).take(pageable.getPageSize());
+    }
+
+    @Override
+    public Mono<UserDTO> save(UserDTO userDTO) {
+        User user = new User(userDTO);
+        user.setRole(Role.ROLE_USER);
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        return userRepository.save(user).map(UserDTO::new);
+    }
+
+    @Override
+    public Mono<UserDTO> patchUsername(UUID uuid, String username, Mono<String> currentUsername) {
+        return currentUsername.flatMap(current -> userRepository.findById(uuid).flatMap(u -> {
+            if (!u.getUsername().equals(current)) {
+                throw new DifferentUserException("Users are forbidden from changing other Users!");
+            }
+            u.setUsername(username);
+            return userRepository.save(u).flatMap(this::userWithNotes);
+        }));
+    }
+
+    @Override
+    public Flux<Void> deleteById(UUID uuid, Mono<String> currentUsername) {
+        return currentUsername.flatMapMany(username -> userRepository.findById(uuid).flatMapMany(u -> {
+                    if (!u.getUsername().equals(username)) {
+                        throw new DifferentUserException("Users are forbidden from deleting other Users!");
+                    }
+                    return Flux.merge(noteRepository.deleteAllNotesByUser(uuid), userRepository.deleteById(uuid));
+                }
+        ));
+    }
+}
