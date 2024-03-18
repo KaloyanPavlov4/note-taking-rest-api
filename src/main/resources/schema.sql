96af376ee4b0977b442b4fa9aeeca27a93d0095f
KaloyanPavlov4 <135831877+KaloyanPavlov4@users.noreply.github.com>
2024-03-18 23:29:03 +0200

    Update schema.sql

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,2 +1,2 @@
-create table if not exists users(id uuid default random_uuid() primary key, username varchar(255) unique not null, email varchar(255) unique not null,password varchar(255) not null, role varchar(255));
-create table if not exists notes(id uuid default random_uuid() primary key, title varchar(255) not null, text varchar(255), user_Id uuid, foreign key(user_Id) references users(id));
+create table if not exists users(id uuid default random_uuid() primary key, username varchar(20) unique not null, email varchar(255) unique not null,password varchar(20) not null, role varchar(255));
+create table if not exists notes(id uuid default random_uuid() primary key, title varchar(255) not null, text varchar(5000), user_Id uuid, foreign key(user_Id) references users(id));
