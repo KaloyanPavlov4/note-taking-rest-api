8f98a1dca6e1e140224bfca5b5d59a3056329094
Kaloyan <kaloyanpavlov4@gmail.com>
2024-03-19 17:23:48 +0200

    Move @Secured annotation to class level

--- src/main/java/com/kaloyan/notetakingapp/controller/AdminController.java
+++ src/main/java/com/kaloyan/notetakingapp/controller/AdminController.java
@@ -15,25 +15,23 @@ import reactor.core.publisher.Mono;
 import java.util.UUID;
 
 @RestController
+@Secured("ROLE_ADMIN")
 @RequestMapping("/admin")
 public class AdminController {
 
     @Autowired
     AdminService adminService;
 
-    @Secured("ROLE_ADMIN")
     @DeleteMapping("/users/{id}")
     public Flux<Void> deleteUser(@PathVariable("id") UUID userId) {
         return adminService.deleteUser(userId);
     }
 
-    @Secured("ROLE_ADMIN")
     @DeleteMapping("/notes/{id}")
     public Mono<Void> deleteNote(@PathVariable("id") UUID noteId) {
         return adminService.deleteNote(noteId);
     }
 
-    @Secured("ROLE_ADMIN")
     @PostMapping("/users/{id}")
     public Mono<UserDTO> makeUserAdmin(@PathVariable("id") UUID userId) {
         return adminService.makeUserAdmin(userId);
